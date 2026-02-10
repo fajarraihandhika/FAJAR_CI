b7f3a92760c66278524e25ade5b4ec4b01d19d07
fajarraihandhika <fajar.raihandhika123@gmail.com>
2026-02-10 15:49:07 +0700

    latihan2
    menambahkan function pada kontroller hello

--- application/controllers/hello.php
+++ application/controllers/hello.php
@@ -1,5 +1,14 @@
 <?php
 
 class hello extends CI_Controller{
-    
+    public function index()
+    {
+        $data['nama'] = "FAJAR RAIHANDHIKA GANTENG";
+        $this->load->view('hello_view',$data);
+    }
+    public function namasaya()
+    {
+        $data['nama'] = "Adit";
+        $this->load->view('hello_view',$data);
+    }
 }
